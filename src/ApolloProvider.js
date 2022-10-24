fa728c40b1ef0e47ab74abcdb545e07aef14fa11
Beefs4000 <103645633+Beefs4000@users.noreply.github.com>
2022-10-24 10:42:18 +0800

    added proxy and url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001'
+  uri: 'https://obscure-eyrie-30204.herokuapp.com/'
 });
 
 // takes request and previous context to get data
